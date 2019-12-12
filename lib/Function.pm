2d0ee504313107b5fb30be301ffb182513088a33
ESIM59 <etienne.simonneau@gmail.com>
2019-12-12 19:14:21 +0100

    ajout Function.pm

---
+++ lib/Function.pm
@@ -0,0 +1,62 @@
+#!/bin/perl -w
+
+use Cwd;
+use warnings;
+use strict;
+
+sub Init
+{
+	return;
+}
+
+1;
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+Function.pm [unix] (00:59 01/01/1970)                                                                                                                                                 0,0-1 Tout
+"Function.pm" [Nouveau fichier]
+
